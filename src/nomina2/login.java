bc7d6f6f1b2cb0f7afe8f119d9a180255f0ae66d
Carkam <caanco@hotmail.es>
2019-03-10 13:15:25 -0600

    modificacion de login

--- src/nomina2/login.java
+++ src/nomina2/login.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nomina2;
 
 /**
  *
- * @author Miguel Angel Chamizo
+ * @author ricardo perez 1255
+ * es el login del programa, con base de datos
  */
 public class login extends javax.swing.JFrame {
 
